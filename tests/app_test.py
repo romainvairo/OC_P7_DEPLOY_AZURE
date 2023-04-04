39be396b828986a4efde0a3bf85efaaa8c67bdec
romainvairo <romain.vairo@hotmail.com>
2023-04-04 02:34:15 +0200

    add files test

---
+++ tests/app_test.py
@@ -0,0 +1,20 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+import sys
+sys.path.insert(0, "../")
+from enum import Enum
+import urllib
+import json
+import requests
+import aiounittest
+from typing import Dict
+from datetime import datetime
+import app
+
+def test_on_prediction_return_value(self):
+        res = app.predict_sentiment("happy")
+        assert res
+
+def test_on_prediction_return_empty(self):
+        res = app.predict_sentiment(0)
+        assert res
